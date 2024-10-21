8df8f8cd21dd816361b279aa0b94b35d76c46a32
bsnisar <bogdan.sns@gmail.com>
2024-10-21 17:56:00 +0100

    Next changes

--- app/page.tsx
+++ app/page.tsx
@@ -1,68 +1,82 @@
 import Image from "next/image";
+import { Mail, File, AppWindowMac, Globe } from "lucide-react";
 
 export default function Home() {
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
       <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="https://nextjs.org/icons/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              app/page.tsx
-            </code>
-            .
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
+        <div className="self-center	">
+          <Image
+            className="dark:invert"
+            src="logo.svg"
+            alt="logo"
+            width={180}
+            height={38}
+            priority
+          />
+        </div>
+        <div>
+          <div className="text-block" id="header">
+            <p className="text-xl">
+              Overvu Solutions is an AI consulting focused on generative
+              technologies. Founded by AI experts from Revolut, Google,
+              Techstart, PwC, and Cambridge. We believe in the power of AI to
+              unlock creativity across all industries.
+            </p>
+          </div>
+        </div>
 
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="https://nextjs.org/icons/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
+        <div className="container mt-28">
+          <div className="section flex flex-col">
+            <div className="p-0 flex justify-center">
+              <img src="i/pavel.jpg" className="rounded-full w-36 h-36" />
+            </div>
+            <div className="description">
+              <a href="https://www.linkedin.com/in/pgrach/" className="underline underline-offset-2 text-lg">Pavel Grachev</a> <br className="py-1"/>
+              <div className="light">Partner &amp; CEO</div>
+            </div>
+          </div>
+
+          <div className="section flex flex-col">
+            <div className="p-0 flex justify-center">
+              <img src="i/bohdan.jpg" className="rounded-full w-36 h-36 " />
+            </div>
+            <div className="description">
+              <a href="https://www.linkedin.com/in/bsnisar/" className="underline underline-offset-2">Bohdan Snisar</a> <br />
+              <div className="light">Partner &amp; CTO</div>
+            </div>
+          </div>
+
+          <div className="section flex flex-col">
+            <div className="p-0 flex justify-center">
+              <img src="i/yaniv.jpg" className="rounded-full w-36 h-36" />
+            </div>
+            <div className="description">
+              <a href="https://www.linkedin.com/in/yaniv-proselkov/" className="underline underline-offset-2">Yaniv Proselkov</a> <br />
+              <div className="light">Researcher &amp; CSO</div>
+            </div>
+          </div>
+          
         </div>
       </main>
+
       <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
+        <a
+          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
+          href="mailto:partners@overvu.solutions"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <Mail size={16} />
+          Mail
+        </a>
         <a
           className="flex items-center gap-2 hover:underline hover:underline-offset-4"
           href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
           target="_blank"
           rel="noopener noreferrer"
         >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
+          <File size={16} />
           Learn
         </a>
         <a
@@ -71,13 +85,7 @@ export default function Home() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
+          <AppWindowMac size={16} />
           Examples
         </a>
         <a
@@ -86,14 +94,8 @@ export default function Home() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
+          <Globe size={16} />
+          Company
         </a>
       </footer>
     </div>
---
+++ components/Logo.tsx
@@ -0,0 +1,9 @@
+import Image from "next/image";
+
+export default function Logo() {
+  return (
+    <div className="">
+        <Image src={"./logo.svg"} alt="Logo"/>
+      </div>
+  );
+}
